67f347164c4665cef6bc230891b64440dcd71ed2
Kevin Hong <hong.w.kevin@gmail.com>
2024-04-29 17:16:57 -0700

    Add mongoose

---
+++ models/Item.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+export interface Items extends mongoose.Document {
+  name: string;
+  image_url: string;
+  store_id: mongoose.Types.ObjectId;
+  status: 'active' | 'completed';
+  created_at: Date;
+  updated_at: Date;
+  deleted_at: Date;
+}
+
+const ItemSchema = new mongoose.Schema<Items>({
+  name: {
+    type: String,
+    required: [true, "Please provide a name for this item."],
+    maxlength: [60, "Name cannot be more than 50 characters"],
+  },
+  image_url: {
+    required: false,
+    type: String,
+  },
+  store_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Store'
+  },
+  status: {
+    type: String,
+    required: [true, 'Please provide a status for this item']
+  },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+  deleted_at: { type: Date, default: null },
+});
+
+export default mongoose.models.Item || mongoose.model<Items>("Item", ItemSchema);
---
+++ models/Store.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export interface Stores extends mongoose.Document {
+  name: string;
+  deleted_at: Date;
+  updated_at: Date;
+  created_at: Date;
+}
+
+const StoreSchema = new mongoose.Schema<Stores>({
+  name: {
+    type: String,
+    required: [true, "Please provide a name for this item."],
+    maxlength: [60, "Name cannot be more than 50 characters"],
+  },
+  created_at: { type: Date, default: Date.now },
+  updated_at: { type: Date, default: Date.now },
+  deleted_at: { type: Date, default: null },
+});
+
+export default mongoose.models.Store || mongoose.model<Stores>("Store", StoreSchema);
