8facfac399ec59068e83bd8017f196e44ac93db6
Kevin Hong <hong.w.kevin@gmail.com>
2024-04-30 12:58:10 -0700

    add prettier

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,31 @@
-
 import nextPWA from "next-pwa";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // Enable React strict mode for improved error handling
-    swcMinify: true,      // Enable SWC minification for improved performance
-    compiler: {
-      removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
-    },
-    async redirects() {
-      return [
-        // Basic redirect
-        {
-          source: '/',
-          destination: '/stores',
-          permanent: true,
-        },
-      ]
-    },
-  };
+  reactStrictMode: true, // Enable React strict mode for improved error handling
+  swcMinify: true, // Enable SWC minification for improved performance
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
+  },
+  async redirects() {
+    return [
+      // Basic redirect
+      {
+        source: "/",
+        destination: "/stores",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-// Configuration object tells the next-pwa plugin 
+// Configuration object tells the next-pwa plugin
 const withPWA = nextPWA({
-    dest: "public", // Destination directory for the PWA files
-    disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
-    register: true, // Register the PWA service worker
-    skipWaiting: true, // Skip waiting for service worker activation
-  });
-  
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+});
+
 // Export the combined configuration for Next.js with PWA support
 export default withPWA(nextConfig);
